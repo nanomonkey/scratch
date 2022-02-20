959d02ccaab74cfb1c947fe593ef8e74248c29bd
nanomonkey <scottrobertgarrison@gmail.com>
2022-02-20 11:53:32 -0800

    emacs local variables for project

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((nil . ((cider-default-cljs-repl . shadow)
+         (cider-preferred-build-tool . shadow-cljs)
+         (cider-shadow-default-options . ":server")
+         (cider-shadow-watched-builds . (":server" ":client")))))
